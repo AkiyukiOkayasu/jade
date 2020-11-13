49b1616a1abc259e05dfe295f4e6ce73276bb9c4
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-11-14 02:16:37 +0900

    Remove: 使わなくなったコードを削除

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,6 @@
 
 UsbMidiParser midiParser;
 
-void noteOn(byte channel, byte pitch, byte velocity)
-{
-  digitalWrite(LED, HIGH);
-}
-
-void noteOff(byte channel, byte pitch, byte velocity)
-{
-  digitalWrite(LED, LOW);
-}
-
 void setup()
 {
   // put your setup code here, to run once:
