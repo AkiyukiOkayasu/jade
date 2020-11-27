43d155d0e858864f4af8e73f0b5d1489175f7caa
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-11-28 01:07:58 +0900

    コード整理

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,11 @@
 
 UsbMidiParser midiParser;
 
-uint8_t noteCounter;
-
 void jfSetup()
 {
   Wire.beginTransmission(ii::JF_ADDR);
   Wire.write(ii::JF_MODE);
-  Wire.write(1); //適当
+  Wire.write(1); //JF mode 1
   Wire.endTransmission();
   delay(1000); //delayMicroseconds(10);に置き換えてもいいかも
 }
@@ -23,7 +21,7 @@ void iiJF()
   Wire.beginTransmission(ii::JF_ADDR);
 #if 1
   Wire.write(ii::JF_VOX);
-  Wire.write(noteCounter); //all channel
+  Wire.write(0); //0: all output
 #else
   Wire.write(JF_NOTE);
 #endif
@@ -32,30 +30,21 @@ void iiJF()
   Wire.write(0xFF); //Vol High
   Wire.write(0xFF); //Vol Low
   Wire.endTransmission();
-  noteCounter++;
-  if (noteCounter > 6)
-  {
-    noteCounter = 1;
-  }
   delay(1); //delayMicroseconds(10);に置き換えてもいいかも
 }
 
 void setup()
 {
   SerialUSB.begin(9600);
-  noteCounter = 1;
   Wire.begin(); // I2C setup
   pinMode(LED, HIGH);
   digitalWrite(LED, HIGH);
-  //jfSetup();
-  SerialUSB.println("hoge");
   delay(1000);
 }
 
 void loop()
 {
   midiEventPacket_t rx;
-  //iiJF();
   do
   {
     rx = MidiUSB.read();
