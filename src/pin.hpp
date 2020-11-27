9a6074b20717037141f4437b8e8b3cddd40880f0
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-11-28 01:39:03 +0900

    pin定数の名前空間追加

--- src/pin.hpp
+++ src/pin.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
-constexpr int LED = 13;
+namespace pin
+{
+    constexpr uint_fast8_t LED = 13;
+    constexpr uint_fast8_t I2C_SDA = 4;
+    constexpr uint_fast8_t I2C_SCL = 5;
+} // namespace pin
