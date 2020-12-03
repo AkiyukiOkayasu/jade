3f39036a72d074f113c4f8985190ce6a0473e84f
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-12-04 00:56:50 +0900

    Remove: jfSetup(), iiJF()を削除

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,6 @@
 
 UsbMidiParser midiParser;
 
-void jfSetup()
-{
-  Wire.beginTransmission(ii::JF_ADDR);
-  Wire.write(ii::JF_MODE);
-  Wire.write(1); //JF mode 1
-  Wire.endTransmission();
-  delay(1000); //delayMicroseconds(10);に置き換えてもいいかも
-}
-
-void iiJF()
-{
-  Wire.beginTransmission(ii::JF_ADDR);
-#if 1
-  Wire.write(ii::JF_VOX);
-  Wire.write(0); //0: all output
-#else
-  Wire.write(JF_NOTE);
-#endif
-  Wire.write(0x00); //Pitch high
-  Wire.write(0xFF); //Pitch low
-  Wire.write(0xFF); //Vol High
-  Wire.write(0xFF); //Vol Low
-  Wire.endTransmission();
-  delay(1); //delayMicroseconds(10);に置き換えてもいいかも
-}
-
 void setup()
 {
   SerialUSB.begin(115200);
