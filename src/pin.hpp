d673c1cc3134df435fbf1c0b9a56e4d7f8d8cfb1
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-02-11 22:06:23 +0900

    Seeeduino XAIOのGPIO定数

--- src/pin.hpp
+++ src/pin.hpp
@@ -1,8 +1,15 @@
+/** 
+    Constants about Seeeduino XAIO GPIO
+    @file pin.hpp
+    @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+    @copyright Copyright (c) 2021 - Akiyuki Okayasu    
+*/
+
 #pragma once
 
 namespace pin
 {
-    constexpr uint_fast8_t LED = 13;
-    constexpr uint_fast8_t I2C_SDA = 4;
-    constexpr uint_fast8_t I2C_SCL = 5;
+constexpr uint_fast8_t LED = 13;
+constexpr uint_fast8_t I2C_SDA = 4;
+constexpr uint_fast8_t I2C_SCL = 5;
 } // namespace pin
