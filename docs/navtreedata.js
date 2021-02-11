32a8eafc221ca2f302d61d2718dd649122837da5
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-02-11 22:39:07 +0900

    doxygen

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "toki", "index.html", [
+  [ "jade", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ]
     ] ],
