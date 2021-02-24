2d4bc553bd9be6fbb773139adea805a27cffa5a7
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-02-25 00:47:58 +0900

    comments

--- src/MIDI/MidiConstants.hpp
+++ src/MIDI/MidiConstants.hpp
@@ -9,34 +9,34 @@
 
 #include <cstdint>
 
-/** USB-MIDI CIN (Code Index Number)
+/** USB-MIDI CIN (Code Index Number).
     https://pcm1723.hateblo.jp/entry/20150109/1420819489
 */
 namespace CIN
 {
-constexpr uint8_t SYSCOMMON_2BYTES = 0x02; // 2Byte
+constexpr uint8_t SYSCOMMON_2BYTES = 0x02; ///< データ長2Byte
 constexpr uint8_t SYSCOMMON_3BYTES = 0x03;
 constexpr uint8_t SYSEX_START_OR_CONTINUE = 0x04;
-constexpr uint8_t SYSEX_END_1BYTE = 0x05;  // 1Byte
-constexpr uint8_t SYSEX_END_2BYTES = 0x06; // 2Byte
-constexpr uint8_t SYSEX_END_3BYTES = 0x07;
+constexpr uint8_t SYSEX_END_1BYTE = 0x05;  ///< SysEx最終パケット データ長1Byte
+constexpr uint8_t SYSEX_END_2BYTES = 0x06; ///< SysEx最終パケット データ長2Byte
+constexpr uint8_t SYSEX_END_3BYTES = 0x07; ///< SysEx最終パケット データ長3Byte
 constexpr uint8_t NOTE_OFF = 0x08;
 constexpr uint8_t NOTE_ON = 0x09;
 constexpr uint8_t POLYPHONIC_KEY_PRESSURE = 0x0A;
 constexpr uint8_t CONTROL_CHANGE = 0x0B;
-constexpr uint8_t PROGRAM_CHANGE = 0x0C;   // 2Byte
-constexpr uint8_t CHANNEL_PRESSURE = 0x0D; // 2Byte
+constexpr uint8_t PROGRAM_CHANGE = 0x0C;   ///< データ長2Byte
+constexpr uint8_t CHANNEL_PRESSURE = 0x0D; ///< データ長2Byte
 constexpr uint8_t PITCH_BEND = 0x0E;
-constexpr uint8_t SYS_REALTIME = 0x0F; // 1Byte
+constexpr uint8_t SYS_REALTIME = 0x0F; ///< データ長1Byte
 } // namespace CIN
 
-/** MIDI SysEx
+/** MIDI SysEx.
     0xF0, 0x7D, 0x7F, ~~~~~ , 0xF7
 */
 namespace SysEx
 {
-constexpr uint8_t BEGIN = 0xF0;
-constexpr uint8_t END = 0xF7;
+constexpr uint8_t BEGIN = 0xF0; ///< SysEx開始
+constexpr uint8_t END = 0xF7;   ///< SysEx終了
 
 namespace ManufacturerID
 {
@@ -47,7 +47,7 @@ namespace ManufacturerID
 
 namespace DeviceID
 {
-    constexpr uint8_t ALL = 0x7F; ///< 全端末
+    constexpr uint8_t ALL = 0x7F;  ///< 全端末
     // TODO jadeのDeviceIDを追加する?
 } // namespace DeviceID
 } // namespace SysEx
