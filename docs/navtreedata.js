47efc80f00fe1d283c22828391cb3b6b1b3884d8
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-02-25 00:56:38 +0900

    doxygen

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,34 +25,48 @@
 var NAVTREE =
 [
   [ "jade", "index.html", [
-    [ "Namespaces", "namespaces.html", [
-      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Variables", "namespacemembers_vars.html", null ]
-      ] ]
-    ] ],
+    [ "CHANNEL_PRESSURE", "namespace_c_i_n.html#ae97167fc16ffc79f1cc311c0129ac295", null ],
+    [ "CONTROL_CHANGE", "namespace_c_i_n.html#a6c9ca815d16e90e0a758a716ef9a7d3e", null ],
+    [ "NOTE_OFF", "namespace_c_i_n.html#a3b1a62ef705205ae714623a40e25302a", null ],
+    [ "NOTE_ON", "namespace_c_i_n.html#a90e05237b3ee2bad82033bdd7e1ee12d", null ],
+    [ "PITCH_BEND", "namespace_c_i_n.html#aee20029b26bd302f72413960607fca1a", null ],
+    [ "POLYPHONIC_KEY_PRESSURE", "namespace_c_i_n.html#a205a26c97c952d1bdf1606e21e27efd6", null ],
+    [ "PROGRAM_CHANGE", "namespace_c_i_n.html#a2fde08d1725813144398e515e6e2bdc7", null ],
+    [ "SYS_REALTIME", "namespace_c_i_n.html#ae1924f325d574951655f6cbaefd2ba1b", null ],
+    [ "SYSCOMMON_2BYTES", "namespace_c_i_n.html#aa029c6af39b6c67b694a22e24128fc48", null ],
+    [ "SYSCOMMON_3BYTES", "namespace_c_i_n.html#a178fb6dc4a80e7f0d3c0db9124406cab", null ],
+    [ "SYSEX_END_1BYTE", "namespace_c_i_n.html#a22808258139db677fa986c04690af0e4", null ],
+    [ "SYSEX_END_2BYTES", "namespace_c_i_n.html#aa73065a5e40196dd6f5ea590759e02ec", null ],
+    [ "SYSEX_END_3BYTES", "namespace_c_i_n.html#afd21c608516ec74842066d24c3a716aa", null ],
+    [ "SYSEX_START_OR_CONTINUE", "namespace_c_i_n.html#a850b1ba5317c2ab3f38a99294b793016", null ],
+    [ "Note", "struct_m_i_d_i_1_1_note.html", "struct_m_i_d_i_1_1_note" ],
+    [ "ControlChange", "struct_m_i_d_i_1_1_control_change.html", "struct_m_i_d_i_1_1_control_change" ],
+    [ "CLOCK_OUT", "pin_8hpp.html#a0a9134b968fdf393c74a00cf827fb5ef", null ],
+    [ "GATE_OUT", "pin_8hpp.html#a5a7ea16e508a1d30b24cc68b1208cec9", null ],
+    [ "I2C_SCL", "pin_8hpp.html#ae47339b8757049761604b6d92eaf8179", null ],
+    [ "I2C_SDA", "pin_8hpp.html#a8b410b2aae5d93f3b2a8b8a136e4e379", null ],
+    [ "LED", "pin_8hpp.html#a09c50164a10660c54ed110c8f9d8ea9c", null ],
+    [ "ALL", "_midi_constants_8hpp.html#ac7fcbcbf49e8d2d54e40f30e7b21aa69", null ],
+    [ "JADE", "_midi_constants_8hpp.html#a23ed785a6165f1c75c7bed93f741fbd9", null ],
+    [ "NON_COMMERCIAL", "_midi_constants_8hpp.html#a1a71abb82df612df59af7716e806fe52", null ],
+    [ "NON_REALTIME", "_midi_constants_8hpp.html#a5018affcd5879d24dbc4cfe092e25fb3", null ],
+    [ "REALTIME", "_midi_constants_8hpp.html#a087c47c9475696e16fd6e34aa619b52b", null ],
+    [ "BEGIN", "namespace_sys_ex.html#a2335916458251039d36e7e5c9e56847f", null ],
+    [ "END", "namespace_sys_ex.html#aa5f15a89040e011994356d89063eefc6", null ],
+    [ "CLOCK", "main_8cpp.html#a3777c22d1f4db1f6c8f8eab2faae9895", null ],
+    [ "PRESCALER", "main_8cpp.html#af81d0254097ce1c433ebcc1a00b87a5f", null ],
     [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
+      [ "Class List", "annotated.html", "annotated_dup" ]
     ] ],
     [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ]
-      ] ]
+      [ "File List", "files.html", "files_dup" ]
     ] ]
   ] ]
 ];
 
 var NAVTREEINDEX =
 [
-""
+"_midi_constants_8hpp.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
