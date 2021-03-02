8cd23d3bc89ce70b96faa729ec6bc9c9640dc76d
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-03-03 01:50:55 +0900

    pin5と7の割り込みは同時に使えないので、pin7を使うのをやめ、GATE_INは4個に減らした

--- src/pin.hpp
+++ src/pin.hpp
@@ -15,8 +15,7 @@ constexpr uint8_t I2C_SCL = 5;
 constexpr uint8_t CLOCK_OUT = 2; ///< D2
 constexpr uint8_t GATE_OUT = 3;  ///< D3
 constexpr uint8_t GATE_IN1 = 6;  ///< D6
-constexpr uint8_t GATE_IN2 = 7;  ///< D7
-constexpr uint8_t GATE_IN3 = 8;  ///< D8
-constexpr uint8_t GATE_IN4 = 9;  ///< D9
-constexpr uint8_t GATE_IN5 = 10; ///< D10
+constexpr uint8_t GATE_IN2 = 8;  ///< D8
+constexpr uint8_t GATE_IN3 = 9;  ///< D9
+constexpr uint8_t GATE_IN4 = 10; ///< D10
 } // namespace pin
