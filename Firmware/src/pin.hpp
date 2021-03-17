c9bca67d699e07fa79d803ef6a9de2870d6ba197
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-03-18 01:03:59 +0900

    Clock outとGate outのGPIOが入れ替わってたのを修正

--- Firmware/src/pin.hpp
+++ Firmware/src/pin.hpp
@@ -12,8 +12,8 @@ namespace pin
 constexpr uint8_t LED = 13;
 constexpr uint8_t I2C_SDA = 4;
 constexpr uint8_t I2C_SCL = 5;
-constexpr uint8_t CLOCK_OUT = 2; ///< D2
-constexpr uint8_t GATE_OUT = 3;  ///< D3
+constexpr uint8_t CLOCK_OUT = 3; ///< D3
+constexpr uint8_t GATE_OUT = 2;  ///< D2
 constexpr uint8_t GATE_IN1 = 6;  ///< D6
 constexpr uint8_t GATE_IN2 = 8;  ///< D8
 constexpr uint8_t GATE_IN3 = 9;  ///< D9
